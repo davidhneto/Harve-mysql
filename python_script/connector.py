7e1c3fa7e18c4bbea9c560579b8459700ecea756
davidhneto <dhn1974@hotmail.com>
2024-01-17 21:28:06 -0300

    Commitando depois de muiiito tempo

---
+++ python_script/connector.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+    user='root',
+    password= 'root',
+    host='127.0.0.1',
+    database= 'meu',
+    port='3306'
+)
+
+if conn.is_connected():
+    print("CONECTAMOS !!!!!")
